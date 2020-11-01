7f752576611a2cc04f67739eea336a6c7c4d6878
Amares101600 <71235898+Amares101600@users.noreply.github.com>
2020-11-01 16:52:06 -0600

    Update until_loop.sh

--- until_loop.sh
+++ until_loop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+#This is the until loop, the code executes until the conditon statement does not fail
 counter=6
-until [ $counter -lt 3 ]; do
-	let counter-=1
-	echo $counter
+until [ $counter -lt 3 ]; do #The condition in this case is "until counter is 3"
+	let counter-=1 #subtract - from the counter each iteration
+	echo $counter#output the counter value
 done
