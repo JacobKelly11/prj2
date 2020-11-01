abb739a711a716f3b79204fc5c829f7d3808a901
Amares101600 <71235898+Amares101600@users.noreply.github.com>
2020-11-01 16:49:24 -0600

    Update for_loop.sh

--- for_loop.sh
+++ for_loop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
-for i in 1 2 3; do
+#This is the for loop, it iterates a certain number of times as defined in the loop conditon statement
+for i in 1 2 3; do #The condition statement is peculiar since most for statements compare the variable to a number to define that number of iterations, rather than saying in ... number
 	echo $i
 done
